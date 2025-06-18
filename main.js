973242ed195ff5646e39f6e196619df41a7b9552
VinhNguyenZ3 <thvinh951@gmail.com>
2025-06-18 11:55:32 +0700

    Tính năng sửa, done, delete

--- main.js
+++ main.js
@@ -1,20 +1,68 @@
-const tasks = [
-  {
-    title: "Nấu cơm",
-    completed: true,
-  },
-  {
-    title: "Rửa bát",
-    completed: true,
-  },
-  {
-    title: "Quét nhà",
-    completed: true,
-  },
-];
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-const html = tasks.map(
-  (task) => `<li class="task-item ${task.completed ? "completed" : ""}">
+const tasks = [];
+const taskList = $("#task-list");
+const todoForm = $(".todo-form");
+const todoInput = $("#todo-input");
+const todoEdit = $("#todo-input");
+
+function addTask(e) {
+  e.preventDefault();
+
+  const value = todoInput.value.trim();
+  if (!value) {
+    todoInput.value = "";
+    return alert("Please write something!");
+  }
+
+  tasks.push({
+    title: value,
+    completed: false,
+  });
+  renderTasks();
+  todoInput.value = "";
+}
+
+function handleTaskActions(e) {
+  e.preventDefault();
+  const itemTodo = e.target.closest(".task-item");
+  const taskIndex = itemTodo.getAttribute("task-index");
+  const task = tasks[taskIndex];
+
+  if (e.target.closest(".edit")) {
+    const newTitle = prompt("Enter the new task title:", task.title).trim();
+
+    if (newTitle === null) return;
+    if (!newTitle) {
+      alert("Task title cannot be empty!");
+      return;
+    }
+
+    task.title = newTitle;
+    renderTasks();
+  } else if (e.target.closest(".done")) {
+    task.completed = !task.completed;
+    renderTasks();
+  } else if (e.target.closest(".delete")) {
+    if (confirm(`Are you sure you want to delete "${task.title}"?`)) {
+      tasks.splice(taskIndex, 1);
+      renderTasks();
+    }
+  }
+}
+
+function renderTasks() {
+  if (!tasks.length) {
+    taskList.innerHTML = `<li class="empty-message">No tasks available.</li>`;
+    return;
+  }
+
+  let html = tasks
+    .map(
+      (task, index) => `<li task-index="${index}" class="task-item ${
+        task.completed ? "completed" : ""
+      }">
         <span class="task-title">${task.title}</span>
         <div class="task-action">
             <button class="task-btn edit">Edit</button>
@@ -24,4 +72,11 @@ const html = tasks.map(
             <button class="task-btn delete">Delete</button>
         </div>
     </li>`
-);
+    )
+    .join("");
+  taskList.innerHTML = html;
+}
+
+renderTasks();
+todoForm.addEventListener("submit", addTask);
+taskList.addEventListener("click", handleTaskActions);
